857d89da1d0463844b17afcdc113dfb472d50412
Jan Niestadt <jan.niestadt@inl.nl>
2018-12-05 12:17:31 +0100

    Invoerveld dat checkt op lengte.

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <h1>Kwetter</h1>
+    <MessageInput ref='messageInput' v-model="newMessage" :maxLength="maxLength" />
+    <button :disabled="!canPostMessage">Verstuur</button>
+  </div>
+</template>
+
+<script>
+
+import MessageInput from './components/MessageInput.vue';
+
+export default {
+
+  // Components die we gebruiken
+  components: {
+    MessageInput
+  },
+
+  // Top-level app state
+  data: function() {
+    return {
+
+      // Maximale lengte van een bericht
+      maxLength: 40,
+
+      // Het nieuwe bericht dat bewerkt wordt
+      newMessage: ""
+    };
+  },
+
+  computed: {
+    // Kan het bericht geplaatst worden, of niet? (leeg, te lang of geen naam gegeven)
+    canPostMessage: function () {
+      return this.newMessage.length > 0 && this.newMessage.length <= this.maxLength;
+    }
+  }
+
+}
+</script>
+
+<style>
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  max-width: 15cm;
+}
+
+h1 {
+  padding: 10px;
+  margin: 0;
+}
+
+button {
+  margin-left: 10px;
+}
+
+</style>
---
+++ src/components/MessageInput.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <textarea 
+      ref="myTextArea"
+
+      placeholder="Wat is er aan de hand?" 
+      autofocus
+      :class="{ invalid: value.length > maxLength }" 
+
+      :value="value"
+      @input="valueChanged" 
+      ></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+
+  // Waardes die dit component ontvangt van de parent.
+  // Niet wijzigen in dit component!
+  props: {
+    value: String,            // nodig om dit component met v-model te laten werken
+    maxLength: Number,
+    placeholderText: String
+  },
+
+  // Methods die je bijv. kunt aanroepen in reactie op events
+  methods: {
+
+    // Wordt aangeroepen als de waarde van user of message verandert
+    // Genereert een input event zodat dit component met v-model werkt
+    valueChanged: function () {
+      this.$emit('input', this.$refs.myTextArea.value);
+    }
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  padding: 10px;
+}
+
+input, textarea {
+  padding: 6px;
+  border: 2px solid #aaf;
+  border-radius: 4px;
+  font-size: 14pt;
+}
+
+textarea {
+  width: 14cm;
+  height: 2cm;
+}
+
+.invalid {
+  background-color: #fdd;
+}
+
+button {
+  font-size: 12pt;
+  margin-top: 10px;
+}
+</style>
